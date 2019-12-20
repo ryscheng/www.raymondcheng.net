d75d8afdfbff249c0cf3ce8d58ad3d3f1db28eb1
Raymond Cheng <me@raymondcheng.net>
2019-12-20 15:01:05 -0800

    add jekyll ignore

---
+++ publish.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+CWD=`pwd`
+REPO_NAME="ryscheng.github.io"
+REPO_URL="git@github.com:ryscheng/ryscheng.github.io.git"
+GIT_PATH=/tmp/$REPO_NAME
+
+echo "Current directory is $CWD"
+
+echo "=== Cloning Repo ==="
+cd /tmp
+if [ -d $GIT_PATH ]; then
+  echo "github:ryscheng.github.io already cloned. Pulling..."
+  cd $REPO_NAME
+  git pull
+else
+  git clone $REPO_URL
+fi
+cd $CWD
+
+echo "=== Committing Changes ==="
