6b4231d3de1a6cf92d7e0949f6e973a1a380ecfb
Raymond Cheng <me@raymondcheng.net>
2023-12-03 17:00:55 -0800

    placeholder text

--- docusaurus/src/pages/index.tsx
+++ docusaurus/src/pages/index.tsx
@@ -2,7 +2,6 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 import { PlasmicRootProvider, PlasmicComponent } from '@plasmicapp/loader-react';
 
@@ -38,9 +37,6 @@ export default function Home(): JSX.Element {
       description="Description will go into a meta tag in <head />">
       <PlasmicRootProvider loader={PLASMIC}>
         <HomepageHeader />
-        <main>
-          <HomepageFeatures />
-        </main>
         <PlasmicComponent component="Homepage" />
       </PlasmicRootProvider>
     </Layout>
